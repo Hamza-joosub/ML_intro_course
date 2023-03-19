403838b4985a3ede34cd91a1ef0d4e0c51ede11b
Hamza-joosub <125697787+Hamza-joosub@users.noreply.github.com>
2023-03-19 01:37:11 +0000

    re-structure

--- Exercises/Course2/forwardPropogation.py
+++ Exercises/Course2/forwardPropogation.py
@@ -1,10 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.layers import Dense
 from tensorflow.keras import Sequential
-from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
 from tensorflow.keras.activations import sigmoid
-from keras.models import Model
 
 
 x = np.array([[200.0, 17.0, 60.0, 120.0]]) 
