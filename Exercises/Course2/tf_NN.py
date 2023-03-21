9de8e49b58a64bff25082469a6e83a66b73e3592
Hamza-joosub <125697787+Hamza-joosub@users.noreply.github.com>
2023-03-21 02:01:49 +0200

    back prop

--- Exercises/Course2/tf_NN.py
+++ Exercises/Course2/tf_NN.py
@@ -9,24 +9,23 @@ tf.autograph.set_verbosity(0)
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
 
-
 #data
-X = np.array([[1,7],[28,3],[1,5],[7,13],[8,40],[13,8],[18,6],[2,14],[3,11],[4,21]]) 
-Y = np.array([[1],[0],[0],[0], [1],[0],[1],[0],[1], [0]])
+X = np.array([[69527371, 30.7075],[76140310, 22.2051],[7549, 51.33],[84854656, 146.6651],[1331, 109.78],[50141098,23.9036],[98944633, 26.429]]) 
+Y = np.array([[0.011694],[0.013753],[-0.027954],[0.007204], [0.008152],[-0.045479],[-0.005454]])
 print(f"x data shape = {X.shape} and y shape = {Y.shape}")
 
 #normalization
-print(f"pre-norm-max/min-temp {np.max(X[:,0]):0.2f}, {np.min(X[:,0]):0.2f}")
-print(f"pre-Norm-max/min-time: {np.max(X[:,1]):0.2f}, {np.min(X[:,1]):0.2f}")
-norm_l = tf.keras.layers.Normalization(axis=-1)
+print(f"pre-norm-max/min-temp {np.max(X[:,0]):0.2f}, {np.min(X[:,0]):0.2f}\n")
+print(f"pre-Norm-max/min-time: {np.max(X[:,1]):0.2f}, {np.min(X[:,1]):0.2f}\n")
+norm_l = tf.keras.layers.Normalization()
 norm_l.adapt(X)  # learns mean, variance
 Xn = norm_l(X)
 print(f"post-norm-max/min-temp {np.max(Xn[:,0]):0.2f}, {np.min(Xn[:,0]):0.2f}")
 print(f"post-Norm-max/min-time: {np.max(Xn[:,1]):0.2f}, {np.min(Xn[:,1]):0.2f}")
 
 #copy data 
-Xt = np.tile(Xn,(1000,1))
-Yt= np.tile(Y,(1000,1))   
+Xt = np.tile(Xn,(5000,1))
+Yt= np.tile(Y,(5000,1))   
 print(Xt.shape, Yt.shape) 
 
 #create Model
@@ -34,8 +33,9 @@ tf.random.set_seed(1234)  # applied to achieve consistent results
 model = Sequential(
     [
         tf.keras.Input(shape=(2,)),
-        Dense(3, activation='sigmoid', name = 'layer1'),
-        Dense(1, activation='sigmoid', name = 'layer2')
+        Dense(25, activation='relu', name = 'layer1'),
+        Dense(15, activation='relu', name = 'layer2'),
+        Dense(1, activation='relu', name = 'layer3')
      ]
 )
 model.summary()
@@ -49,14 +49,11 @@ print(f"W2{W2.shape}:\n", W2, f"\nb2{b2.shape}:", b2)
 #compile model
 model.compile(
     loss = tf.keras.losses.BinaryCrossentropy(),
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.05),
 )
 
-#fit model
-model.fit(
-    Xt,Yt,            
-    epochs=10,
-)
+#Back Propogation
+model.fit(Xt,Yt,epochs=100)
 
 #get updated weights and biases
 W1, b1 = model.get_layer("layer1").get_weights()
@@ -64,10 +61,8 @@ W2, b2 = model.get_layer("layer2").get_weights()
 print("W1:\n", W1, "\nb1:", b1)
 print("W2:\n", W2, "\nb2:", b2)
 
-#inference
-X_test = np.array([
-    [188,11],  # postive example
-    [210,15]])   # negative example
+#Forward Propogation
+X_test = np.array([[87300242,97.0362]])  # postive example y = -0,010896])   # negative example
 X_testn = norm_l(X_test)
 predictions = model.predict(X_testn)
 print("predictions = \n", predictions)
